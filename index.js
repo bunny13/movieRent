81527e84112eb80d9ebc4ffbbce761d90105df12
shashank.verma <shashank.verma>
2019-09-20 12:32:21 -0400

    Added authentication

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const genres = require('./routes/genres');
 const customer = require('./routes/customer');
 const movie = require('./routes/movies');
 const rent = require('./routes/rent');
+const register = require('./routes/register');
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/genres')
@@ -16,6 +17,7 @@ app.use('/api/genres',genres);
 app.use('/api/customer',customer);
 app.use('/api/movies',movie);
 app.use('/api/rent',rent);
+app.use('/api/register',register);
 
 const port = process.env.PORT || 3000;
 app.listen(port, console.log(`Listening to PORT....:${port }`));
